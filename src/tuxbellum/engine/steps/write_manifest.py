2cd30063de700c598014ee3dd366153f3875bea7
ch3w3y <ch3w3y@users.noreply.github.com>
2026-05-23 09:24:07 +0100

    chore: bump version to 4.0.8

--- src/tuxbellum/engine/steps/write_manifest.py
+++ src/tuxbellum/engine/steps/write_manifest.py
@@ -27,7 +27,7 @@ def step(ctx: InstallContext) -> None:
         dxvk_source = "downloaded"
 
     manifest = InstallManifest.now(
-        tuxbellum_version="4.0.7",
+        tuxbellum_version="4.0.8",
         wineprefix=ctx.wineprefix,
         proton_version=ctx.proton_version,
         proton_path=ctx.proton_path,
